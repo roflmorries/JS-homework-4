92bf6f1fce33c5f6b498eb23a135c082e2e2e5e5
roflmorries <99510874+roflmorries@users.noreply.github.com>
2024-12-23 00:14:55 +0200

    done hm

---
+++ main.js
@@ -0,0 +1,65 @@
+// 1
+
+let nums = ""
+
+for (let i = 20; i <= 30; i += 0.5) {
+    nums += i + " ";
+}
+console.log(nums);
+
+
+// 2.
+
+let usdToUah = 42;
+
+for (let i = 10; i <= 100; i += 10) {
+    let uah = usdToUah * i;
+    console.log(`${i} долларов стоят ${uah}`);
+}
+
+
+// 3.
+
+let num = 172;
+for (let i = 1; i <= 100; i++) {
+    if (i * i <= num) {
+        console.log(i);
+    }
+}
+
+
+// 4.
+
+const userNum = 72;
+
+let isPrime = true;
+
+
+if (!userNum >= 1) {
+    isPrime = false;
+} else {
+    for (let i = 2; i < userNum; i++) {
+        if (userNum % i === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+}
+
+console.log(`${userNum} является простым числом: ${isPrime}`);
+
+
+// 5.
+
+const userNum = 27;
+
+let isPower3rd = false;
+
+for (let i = 1; i <= userNum; i *= 3) {
+    if (i === userNum) {
+        isPower3rd = true;
+        break;
+    }
+}
+
+console.log(`${userNum} можно получить путем возведения числа 3 в степень: ${isPower3rd}`);
---
+++ practice.js
@@ -0,0 +1,134 @@
+// 1.
+
+let numbers = "";
+
+for (let i = 10; i <= 20; i++) {
+    numbers += i;
+
+    if (i < 20) {
+        numbers += ", ";
+    }
+
+}
+document.write(numbers);
+
+
+// 2.
+
+let numbers = "";
+
+for (let i = 10; i <= 20; i++) {
+    numbers += i * i;
+
+    if (i < 20) {
+        numbers += ", ";
+    }
+
+}
+document.write(numbers);
+
+
+// 3.
+
+let numbers = "";
+
+for (let i = 1; i <= 10; i++) {
+    numbers += `${i} * 7 = ${i * 7}<br>`;
+}
+document.write(numbers)
+
+
+// 4.
+
+let sum = 0;
+
+for (let i = 1; i <= 15; i++) {
+    sum += i;
+}
+document.write(`Сумма всех целых чисел от 1 до 15: ${sum}`)
+
+
+// 5.
+
+let sum = BigInt(1);
+
+for (let i = 15; i <= 35; i++) {
+
+    if (Number.isInteger(i)) {
+        sum *= BigInt(i);
+    }
+}
+document.write(`Произведение всех чисел от 15 до 35: ${sum}`)
+
+
+// 6.
+
+let sum = 0;
+let counter = 0;
+
+for (let i = 1; i <= 500; i++) {
+    sum += i;
+    counter++;
+}
+
+let arithmeticAverage = sum / counter;
+
+alert(`${arithmeticAverage} является средним арифметическим всех целых чисел от 1 до 500.`);
+
+
+// 7.
+
+let sum = 0;
+
+for (let i = 30; i <= 80; i++) {
+    if (i % 2 === 0) {
+        sum += i;
+    }
+}
+
+alert(`${sum} - сумма парных чисел от 30 до 80.`)
+
+
+// 8.
+
+for (let i = 100; i <= 200; i++) {
+    if (i % 3 === 0) {
+        console.log(i)
+    }
+}
+
+
+// 9.
+
+const userNum = 28;
+
+let divisorCount = 0;
+let divisorSum = 0;
+
+console.log(`Все делители числа ${userNum}:`);
+
+for (let i = 1; i <= userNum; i++) {
+    if (userNum % i === 0) {
+        console.log(i + '');
+
+        if (i % 2 === 0) {
+            divisorCount++;
+            divisorSum += i;
+        }
+    }
+}
+
+console.log(`Количество парных делителей: ${divisorCount}`);
+console.log(`Сумма парных делителей: ${divisorSum}`);
+
+
+// 10.
+
+for (let i = 1; i <= 10; i++) {
+    let row = "";
+
+    for (let j = 1; j <= 10; j++) {
+        row += `${i * j}\t`;
+    }
+    console.log(row);
+}
